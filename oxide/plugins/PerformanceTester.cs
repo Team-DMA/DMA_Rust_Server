58b7f4e8b3b7824cdeb09ee674876953004f2634
bergungsdackel <mateusz-koziel@t-online.de>
2021-03-31 00:09:45 +0200

    map + plugins

---
+++ oxide/plugins/PerformanceTester.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Oxide.Plugins
+{
+    [Info("PerformanceTester", "TeamDMA", "0.0.1")]
+    [Description("Some performance tests")]
+    class PerformanceTester : RustPlugin
+    {
+        void OnPlayerCommand(BasePlayer player, string command, string[] args)
+        {
+            if(args.Length > 0)
+            {
+                Puts(player.displayName + " used the command '/" + command + " " + String.Join(" ", args) + "'");
+            }
+            else
+            {
+                Puts(player.displayName + " used the command '/" + command + "'");
+            }
+        }
+    }
+}
